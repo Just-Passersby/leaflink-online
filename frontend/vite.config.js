c068a1f211c234fd1ee422cf68e1d051060924dd
Lcd0327 <gary3321867@gmail.com>
2026-06-04 14:50:47 +0800

    fix: vite proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			'/auth': 'http://127.0.0.1:8000',
+			'/notes': 'http://127.0.0.1:8000',
+			'/tags': 'http://127.0.0.1:8000',
+			'/vaults': 'http://127.0.0.1:8000',
+			'/search': 'http://127.0.0.1:8000'
+		}
+	}
 });
